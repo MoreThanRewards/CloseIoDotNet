46fe6d4e45a5193971d8ea61ed6a94ab39d06a2a
Tony Lechner <tony@morethanrewards.com>
2016-05-21 21:14:32 -0500

    Unit test for EntityFieldAttribute

---
+++ Tests/CloseIoDotNet.Test/Entities/Fields/LeadEntityFieldAttributeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CloseIoDotNet.Test.Entities.Fields
+{
+    using CloseIoDotNet.Entities.Definitions;
+    using CloseIoDotNet.Entities.Fields;
+
+    [TestClass]
+    public class LeadEntityFieldAttributeTest
+    {
+        [TestMethod]
+        public void TestConstruction()
+        {
+            var unit = new LeadEntityFieldAttribute("ut", "ut", true, true, true, true, true);
+            Assert.AreEqual(typeof (Lead), unit.BelongsTo);
+            Assert.AreEqual("ut", unit.Name);
+            Assert.AreEqual("ut", unit.SerializedName);
+            Assert.IsTrue(unit.IsRequiredOnCreate);
+            Assert.IsTrue(unit.IsRequiredOnUpdate);
+            Assert.IsTrue(unit.IsRequiredOnDelete);
+            Assert.IsTrue(unit.IsAllowedOnCreate);
+            Assert.IsTrue(unit.IsAllowedOnUpdate);
+
+            var unit2 = new LeadEntityFieldAttribute("ut2", "ut2", false, false, false, false, false);
+            Assert.AreEqual(typeof (Lead), unit2.BelongsTo);
+            Assert.AreEqual("ut2", unit2.Name);
+            Assert.AreEqual("ut2", unit2.SerializedName);
+            Assert.IsFalse(unit2.IsRequiredOnCreate);
+            Assert.IsFalse(unit2.IsRequiredOnUpdate);
+            Assert.IsFalse(unit2.IsRequiredOnDelete);
+            Assert.IsFalse(unit2.IsAllowedOnCreate);
+            Assert.IsFalse(unit2.IsAllowedOnUpdate);
+        }
+    }
+}
