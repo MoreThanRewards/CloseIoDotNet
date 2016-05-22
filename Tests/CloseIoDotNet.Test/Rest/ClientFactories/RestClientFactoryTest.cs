eb7227b33afd57ced10bd914d923e25bf1d5dcb9
Tony Lechner <tony@morethanrewards.com>
2016-05-22 17:33:49 -0500

    Normalized test imports.

--- Tests/CloseIoDotNet.Test/Rest/ClientFactories/RestClientFactoryTest.cs
+++ Tests/CloseIoDotNet.Test/Rest/ClientFactories/RestClientFactoryTest.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace CloseIoDotNet.Test.Rest.ClientFactories
+﻿namespace CloseIoDotNet.Test.Rest.ClientFactories
 {
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using CloseIoDotNet.Rest.ClientFactories;
     using RestSharp.Authenticators;
 
--- Tests/CloseIoDotNet.Test/Rest/ResponseEnumerables/ScanEnumeratorTest.cs
+++ Tests/CloseIoDotNet.Test/Rest/ResponseEnumerables/ScanEnumeratorTest.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace CloseIoDotNet.Test.Rest.ResponseEnumerables
+﻿namespace CloseIoDotNet.Test.Rest.ResponseEnumerables
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System.Collections.Generic;
     using System.Linq;
     using System.Net;
